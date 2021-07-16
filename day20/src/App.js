3c9cc41658d49fb5fc3d22dc643b0da189bbef76
Shimanshu yadav <shimanshuyadav83@gmail.com>
2021-07-16 21:03:13 +0530

    update day 20 again

--- day20/src/App.js
+++ day20/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Food from './components/Food'
+import { useState } from "react";
 
 function App() {
+
+
+  const [data , setData] = useState([
+    {name:'Apple',amount:45},
+    {name:'Burger',amount:65},
+    {name:'Chicken',amount:85},
+    {name:'Jalebi',amount:455},
+    {name:'Samosa',amount:465},
+    {name:'Banana',amount:89}
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Instructions</h1>
+      <ul className="instruction">
+        <li> build a container. </li>
+        <li> create a seperate function and use it as a component. </li>
+        <li> pass props "calory and food" and call it to main component. </li>
+      </ul>
+
+      <div className="calorie-container">
+        <h1> Calorie Read Only</h1>
+        <div className="calorie-list">
+        <Food name='Apple' amount='3434' />
+        <Food name='Mango' amount='64' />
+        <Food name='WaterMellon' amount='8434' />
+        <Food name='Grapes' amount='3434' />
+        <Food name='Walnut' amount='334' />
+        <Food name='Pizza' amount='434' />
+
+        </div>
+      </div>
+
     </div>
+
   );
 }
-
-export default App;
